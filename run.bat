65e6031f848cd09cca3e6e0e247eaf705b448a77
claudezhang <claude9493@users.noreply.github.com>
2021-08-23 01:58:15 +0800

    update

---
+++ run.bat
@@ -0,0 +1,3 @@
+@REM python .\src\get_weather.py
+jupyter nbconvert --to notebook --execute ./src/vis_weather.ipynb --inplace
+jupyter nbconvert --TemplateExporter.exclude_input=True ./src/vis_weather.ipynb --output-dir='./' --output='index.html' 
